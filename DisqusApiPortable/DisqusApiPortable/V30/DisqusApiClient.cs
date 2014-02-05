c2aa4f1c43ba45924356b90280bf68f97e2b01c6
ryanvalentin <ryan@disqus.com>
2014-02-04 16:58:21 -0800

    Started adding methods

--- DisqusApiPortable/DisqusApiPortable/V30/DisqusApiClient.cs
+++ DisqusApiPortable/DisqusApiPortable/V30/DisqusApiClient.cs
@@ -6,6 +6,7 @@ using Disqus.Api.V30.Authentication;
 using System.ComponentModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Disqus.Api.V30
 {
@@ -22,5 +23,51 @@ namespace Disqus.Api.V30
         public DsqAuth DisqusAuthentication { get; set; }
 
         private HttpClient _httpClient { get; set; }
+
+        #region Applications endpoints
+
+        public async Task<string> ListUsageAsync(int days = 30)
+        {
+        }
+
+        #endregion
+
+        #region Blacklists endpoints
+
+        #endregion
+
+        #region Forums endpoints
+
+        #endregion
+
+        #region Posts endpoints
+
+        #endregion
+
+        #region Threads endpoints
+
+        #endregion
+
+        #region Users endpoints
+
+        #endregion
+
+        #region Whitelists endpoints
+
+        #endregion
+
+        #region HTTP Client methods
+        
+        private async Task GetDataAsync()
+        {
+
+        }
+
+        private async Task PostDataAsync()
+        {
+
+        }
+
+        #endregion
     }
 }
