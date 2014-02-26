650110a22fddde72e93ce12ee794be239123885e
Ryan Valentin <ryan@disqus.com>
2014-02-26 09:48:47 -0800

    Add string extensions for error

---
+++ DisqusApiPortable/DisqusApiPortable/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Disqus.Api.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string input, int maxLength)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return input.Length <= maxLength ? input : input.Substring(0, maxLength);
+        }
+
+        public static string TruncateWithEllipses(this string input, int maxLength)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return input.Length <= maxLength ? input : input.Substring(0, maxLength) + "...";
+        }
+    }
+}
